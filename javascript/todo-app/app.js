61f382fb87b927b9ce033567393bdc2f31c5cedd
nexumi-ai <menardrush@gmail.com>
2016-04-10 09:00:00 +0800

    add double-click edit with Enter/Escape handling

--- javascript/todo-app/app.js
+++ javascript/todo-app/app.js
@@ -14,7 +14,8 @@ function render() {
   list.innerHTML = '';
   todos.forEach((todo, index) => {
     const li = document.createElement('li');
-    li.className = todo.done ? 'todo done' : 'todo';
+    li.dataset.index = index;
+    li.className = 'todo' + (todo.done ? ' done' : '');
     li.innerHTML = `
       <span class="text">${escapeHtml(todo.text)}</span>
       <div class="actions">
@@ -33,6 +34,32 @@ function escapeHtml(text) {
   return div.innerHTML;
 }
 
+function startEdit(index, item) {
+  const textEl = item.querySelector('.text');
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.value = todos[index].text;
+  editInput.className = 'edit-input';
+  textEl.replaceWith(editInput);
+  editInput.focus();
+  editInput.select();
+
+  let finished = false;
+  const finish = (saveEdit) => {
+    if (finished) return;
+    finished = true;
+    const value = editInput.value.trim();
+    if (saveEdit && value) todos[index].text = value;
+    save();
+  };
+
+  editInput.addEventListener('keydown', (ev) => {
+    if (ev.key === 'Enter') finish(true);
+    if (ev.key === 'Escape') finish(false);
+  });
+  editInput.addEventListener('blur', () => finish(true));
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = input.value.trim();
@@ -49,6 +76,12 @@ list.addEventListener('click', (e) => {
   if (btn.classList.contains('toggle')) todos[index].done = !todos[index].done;
   if (btn.classList.contains('delete')) todos.splice(index, 1);
   save();
+});
+
+list.addEventListener('dblclick', (e) => {
+  const item = e.target.closest('.todo');
+  if (!item) return;
+  startEdit(Number(item.dataset.index), item);
 });
 
 render();
